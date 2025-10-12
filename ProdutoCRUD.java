18ffa8800f45a1786a29e9f334fc7ae17249dfc9
NayronCampos <nayroncampossoares@gmail.com>
2025-10-12 20:17:40 -0300

    Add files via upload
    Crud

---
+++ ProdutoCRUD.java
@@ -0,0 +1,36 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class ProdutoCRUD {
+    private List<Produto> produtos = new ArrayList<>();
+    
+
+    // Criando o produto
+    public void adicionarProduto(String nome, String gtin13, String descricao) {
+        if (gtin13.length() != 13 || !gtin13.matches("\\d{13}")) {
+            System.out.println("GTIN-13 inválido!");
+            return;
+        }
+        Produto p = new Produto(nome, gtin13, descricao);
+        produtos.add(p);
+        System.out.println("Produto adicionado com sucesso!");
+    }
+
+    // Listar os produtos PELO NOME!!
+    public void listarProdutos() {
+        if (produtos.isEmpty()) {
+            System.out.println("Nenhum produto cadastrado.");
+        } else {
+            for (Produto p : produtos) {
+                System.out.println(p.getNome());
+            }
+        }
+    }
+
+    // Deletar
+    public void removerProduto(String nome) {
+        produtos.removeIf(p -> p.getNome().equals(nome));
+        System.out.println("Produto removido (se existia).");
+    }
+}
+ 
